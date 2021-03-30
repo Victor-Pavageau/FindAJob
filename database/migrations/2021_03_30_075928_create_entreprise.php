8b09f34f6730855de2dd87008c1d575b8194069b
Thorwig <mouad.nossair@viacesi.fr>
2021-03-30 19:24:57 +0200

    rien

--- database/migrations/2021_03_30_075928_create_entreprise.php
+++ database/migrations/2021_03_30_075928_create_entreprise.php
@@ -16,13 +16,12 @@ class CreateEntreprise extends Migration
         Schema::create('entreprise', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nom_entreprise');
-            $table->string('domaine_entreprise');
-            $table->string('adresse_entreprise');
-            $table->string('ville_entreprise');
-            $table->string('tel_entreprise');
-            $table->string('zipcode_entreprise');
-            $table->string('pays_entreprise');
-            $table->string('mail_entreprise');
+            $table->string('Secteur');
+            $table->string('Localite');
+            $table->integer('Nombre_de_stagiaire_CESI');
+            $table->integer('Note_globale');
+            $table->integer('Confiance');
+            $table->string('E_mail');
             $table->timestamps();
 
         });
