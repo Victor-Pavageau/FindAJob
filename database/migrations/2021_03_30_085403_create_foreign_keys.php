b02a91bcdd18a2fe76e436a778b11a612cf8c430
Thorwig <mouad.nossair@viacesi.fr>
2021-03-30 12:24:05 +0200

    Migrations a vraiment rien d'etre  finis

--- database/migrations/2021_03_30_085403_create_foreign_keys.php
+++ database/migrations/2021_03_30_085403_create_foreign_keys.php
@@ -21,6 +21,60 @@ class CreateForeignKeys extends Migration
                 ->onDelete('restrict')
                 ->onUpdate('restrict');
         });
+
+        Schema::table('candidature', function (Blueprint $table) {
+            $table->foreign('id_utilisateur')->references('id')->on('utilisateur')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('id_offre_de_stage')->references('id')->on('offre_de_stage')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+
+
+
+        Schema::table('interesser', function (Blueprint $table) {
+            $table->foreign('id_utilisateur')->references('id')->on('utilisateur')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('id_offre_de_stage')->references('id')->on('offre_de_stage')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+
+        Schema::table('offre_de_stage', function (Blueprint $table) {
+            $table->foreign('id_entreprise')->references('id')->on('entreprise')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            
+        });
+
+        Schema::table('posseder', function (Blueprint $table) {
+            $table->foreign('id_utilisateur')->references('id')->on('utilisateur')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('id_competence')->references('id')->on('competence')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+
+        Schema::table('requerir', function (Blueprint $table) {
+            $table->foreign('id_competence')->references('id')->on('competence')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('id_offre_de_stage')->references('id')->on('offre_de_stage')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+
+        Schema::table('viser', function (Blueprint $table) {
+            $table->foreign('id_promotion')->references('id')->on('promotion')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('id_offre_de_stage')->references('id')->on('offre_de_stage')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
     }
 
     /**
