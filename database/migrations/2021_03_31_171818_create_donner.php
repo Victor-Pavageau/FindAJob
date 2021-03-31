bb3736e1d3a5c7be397283ba14659636527c048d
Thorwig <mouad.nossair@viacesi.fr>
2021-03-31 17:26:00 +0200

    table note + table donner

--- database/migrations/2021_03_31_171818_create_donner.php
+++ database/migrations/2021_03_31_171818_create_donner.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProposer extends Migration
+class CreateDonner extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateProposer extends Migration
      */
     public function up()
     {
-        Schema::create('proposer', function (Blueprint $table) {
+        Schema::create('donner', function (Blueprint $table) {
+            $table->integer('id_utilisateur')->unsigned();
             $table->integer('id_entreprise')->unsigned();
-            $table->integer('id_offre_de_stage')->unsigned();
-            $table->foreign('id_entreprise')->references('id')->on('entreprise')
+            $table->foreign('id_utilisateur')->references('id')->on('utilisateur')
                 ->onDelete('restrict')
                 ->onUpdate('restrict');
-            $table->foreign('id_offre_de_stage')->references('id')->on('offre_de_stage')
+            $table->foreign('id_entreprise')->references('id')->on('entreprise')
                 ->onDelete('restrict')
                 ->onUpdate('restrict');
             $table->timestamps();
@@ -33,6 +33,6 @@ class CreateProposer extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('proposer');
+        Schema::dropIfExists('donner');
     }
 }
