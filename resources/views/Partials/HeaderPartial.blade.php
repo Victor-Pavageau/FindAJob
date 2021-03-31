1e3d93ff7d4b47ce1a2c025004ce00768546a751
Mickaël LEBOULANGER <mickael.leboulanger@viacesi.fr>
2021-03-31 11:05:30 +0200

    Header partial fix (route('identification)) pas 'login'

--- resources/views/Partials/HeaderPartial.blade.php
+++ resources/views/Partials/HeaderPartial.blade.php
@@ -5,6 +5,6 @@
     </a>
     <nav class="float-right">
         <a href="{{route('inscription')}}"><button class="float-right btn btn-outline-success my-2 my-sm-0">S'inscrire</button></a>
-        <a href="{{route('login')}}"><button class="float-right btn btn-outline-success mr-sm-4" >Se connecter</button></a>
+        <a href="{{route('identification')}}"><button class="float-right btn btn-outline-success mr-sm-4" >Se connecter</button></a>
     </nav>
 </nav>
